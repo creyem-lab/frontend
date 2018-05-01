347c257258322e017e7b614d24b6b851f6747a58
Andrew Fletcher <andrew.fletcher16@gmail.com>
2018-05-02 00:45:27 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Creyem Lab Frontend
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
