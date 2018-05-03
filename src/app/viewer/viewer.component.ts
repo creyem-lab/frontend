2f207cae61f0ba103b73bff7e1309af571376bcd
Cathan O'Donnell <cathan@elix.sr>
2018-05-03 12:55:47 +0100

    Hotspot updates.

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -27,28 +27,13 @@ export class ViewerComponent implements OnInit {
         .subscribe((hotspots: Hotspot[]) => {
             this.hotspots = hotspots;
             console.log(this.hotspots);
-            this.hotspots.forEach(element => {
-                element.createTooltipFunc = hotspot;
-            }
-            )
             var view = pannellum.viewer('panoramaContainer', {
                 "type": "equirectangular",
                 "panorama": '../../assets/equirect.jpg',
                 "autoLoad": true,
-                "compass": true,
                "hotSpots": this.hotspots
             });
         });
-        // Hot spot creation function
-function hotspot(hotSpotDiv, args) {
-    hotSpotDiv.classList.add('custom-tooltip');
-    var span = document.createElement('span');
-    span.innerHTML = args;
-    hotSpotDiv.appendChild(span);
-    span.style.width = span.scrollWidth - 20 + 'px';
-    span.style.marginLeft = -(span.scrollWidth - hotSpotDiv.offsetWidth) / 2 + 'px';
-    span.style.marginTop = -span.scrollHeight - 12 + 'px';
-}
   }
 
 }
